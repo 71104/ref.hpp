7293d682b33f420c7e048b09d70aa4249bad94b0
Alberto La Rocca <a71104@gmail.com>
2015-05-01 11:46:21 +0200

    Initial commit

---
+++ ref.hpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+
+template<typename T>
+struct ref {
+private:
+	T *m_p;
+public:
+	inline ref() : m_p(0) {}
+	inline ref(T *&&a_rrp) : m_p(std::move(a_rrp)) {}
+	inline ref(ref<T> const&) = delete;
+	inline ref(ref<T> &&a_rrp) : m_p(std::move(a_rrp.m_p)) { a_rrp.m_p = 0; }
+	inline ~ref() { delete m_p; }
+	inline ref<T> &operator = (ref<T> const&) = delete;
+	inline ref<T> &operator = (ref<T> &&a_rrp) { delete m_p, m_p = std::move(a_rrp.m_p), a_rrp.m_p = 0; };
+	inline ref<T> &operator = (T *&&a_rrp) { delete m_p, m_p = std::move(a_rrp); };
+	inline operator T* () { return m_p; }
+	inline operator T const* () const { return m_p; }
+	inline T &operator * () { return *m_p; }
+	inline T const &operator * () const { return *m_p; }
+	inline T *operator -> () { return m_p; }
+	inline T const *operator -> () const { return m_p; }
+};
