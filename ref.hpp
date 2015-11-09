fe1cc0b2c5370cc3bc8a2533b3ccfce5b04bbdb4
Alberto La Rocca <a71104@gmail.com>
2015-11-09 16:36:01 +0000

    Update ref.hpp
    Return value in assignment operators.

--- ref.hpp
+++ ref.hpp
@@ -11,8 +11,8 @@ public:
 	inline ref(ref<T> &&a_rrp) : m_p(std::move(a_rrp.m_p)) { a_rrp.m_p = 0; }
 	inline ~ref() { delete m_p; }
 	inline ref<T> &operator = (ref<T> const&) = delete;
-	inline ref<T> &operator = (ref<T> &&a_rrp) { delete m_p, m_p = std::move(a_rrp.m_p), a_rrp.m_p = 0; };
-	inline ref<T> &operator = (T *&&a_rrp) { delete m_p, m_p = std::move(a_rrp); };
+	inline ref<T> &operator = (ref<T> &&a_rrp) { return delete m_p, m_p = std::move(a_rrp.m_p), a_rrp.m_p = 0, *this; };
+	inline ref<T> &operator = (T *&&a_rrp) { return delete m_p, m_p = std::move(a_rrp), *this; };
 	inline operator T* () { return m_p; }
 	inline operator T const* () const { return m_p; }
 	inline T &operator * () { return *m_p; }
